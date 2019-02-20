39942b7ec0f6906ab7ba9ae88f6ac4148f615059
Luis Fernando Yepes Alvarez - Ceiba Software <yepesluisfernando@hotmail.com>
2019-02-20 10:57:51 -0500

    agrego prueba funcional automatizada con protractor

--- front-end-angular/ceiba-adn/e2e/src/app.e2e-spec.ts
+++ front-end-angular/ceiba-adn/e2e/src/app.e2e-spec.ts
@@ -4,20 +4,41 @@ import { browser, logging } from 'protractor';
 describe('workspace-project App', () => {
   let page: AppPage;
 
+  let placaValida: string;
+
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display a new row for the new vehicle', () => {
+    placaValida = page.generarPlacaAleatoria();
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ceiba-adn!');
+    page.setInputTextPlaca(placaValida);
+    page.clickBotonCarroTipoVehiculo();
+    page.clickBotonIngresar();
+    browser.sleep(5000);
+    const numeroVehiculos = page.getCantidadVehiculosIngresados();
+    expect(numeroVehiculos).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should delete the created vehicle', () => {
+    page.navigateTo();
+    page.clickBotonPagarParqueadero(placaValida);
+    browser.sleep(3000);
+    page.clickBotonRetirarParqueadero(placaValida);
+    browser.sleep(4000);
+    expect(page.existeVehiculoEnParqueadero(placaValida)).toBe(false);
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
+    // const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    // expect(logs).not.toContain(jasmine.objectContaining({
+    //   level: logging.Level.SEVERE,
+    // }));
   });
 });
+
+
+
+
--- front-end-angular/ceiba-adn/e2e/src/app.po.ts
+++ front-end-angular/ceiba-adn/e2e/src/app.po.ts
@@ -1,11 +1,52 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, promise } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  setInputTextPlaca(placa: string) {
+    return element(by.id('placa')).sendKeys(placa);
   }
+
+  clickBotonCarroTipoVehiculo() {
+    element(by.id('carro-button')).click();
+  }
+
+  clickBotonIngresar() {
+    element(by.id('ingresar-button')).click();
+  }
+
+  getCantidadVehiculosIngresados() {
+    return element.all(by.name('nuevo-vehiculo')).count();
+  }
+
+  existeVehiculoEnParqueadero(placa: string) {
+    return element.all(by.id(placa)).isPresent();
+  }
+
+  clickBotonCobrarParqueadero(placa: string) {
+    element(by.id(placa)).element(by.className('btn btn-info')).click();
+  }
+
+  clickBotonPagarParqueadero(placa: string) {
+    element(by.id(placa)).element(by.className('btn btn-success')).click();
+  }
+
+  clickBotonRetirarParqueadero(placa: string) {
+    element(by.id(placa)).element(by.className('btn btn-danger')).click();
+  }
+
+  generarPlacaAleatoria(): string {
+    let placa = '';
+    const numerosPlaca = Math.floor(Math.random() * 999) + 100;
+    const posibleLetra = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = 0; i < 3 ; i++) {
+      placa += posibleLetra.charAt(Math.floor(Math.random() * posibleLetra.length));
+    }
+    placa = placa.concat(numerosPlaca.toString());
+    return placa;
+  }
+
 }
